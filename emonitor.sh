8b0c916d331b07074520cfe5a2d636b5325ef043
RSCLab <pthms.robotics@gmail.com>
2021-07-30 16:30:40 -0500

    Added comments to script

--- emonitor.sh
+++ emonitor.sh
@@ -1,4 +1,10 @@
+echo "Activating environment"
 C:/Users/pthms/anaconda3/Scripts/activate
 conda activate testingenv
+
+echo "Running program"
 python EMonitorPyglet.py
+
+echo "EMonitor Terminated"
+
 $SHELL
